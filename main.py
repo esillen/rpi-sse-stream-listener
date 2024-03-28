803be6dfac10092f354657aaf56957211a7f312d
Erik Sillén <erik.sillen@flowertech.se>
2024-03-28 10:09:13 +0100

    listening to events, turning on/off gpios

---
+++ main.py
@@ -0,0 +1,19 @@
+from time import sleep
+from sseclient import SSEClient
+try:
+    import RPi.GPIO as GPIO
+    
+except RuntimeError:
+    print("Error importing RPi.GPIO! This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+NOTIFICATION_TIME_SECONDS = 10
+URL = 'http://mysite.com/sse_stream/'
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+
+messages = SSEClient(URL)
+for msg in messages:
+    print("new message!")
+    GPIO.output(23, GPIO.HIGH)
+    sleep(NOTIFICATION_TIME_SECONDS)
+    GPIO.output(23, GPIO.LOW)
